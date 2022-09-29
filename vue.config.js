24ec4c5b06daefaf3827524fc54b278dc4589b99
hs <1614419263@qq.com>
2022-09-29 09:52:00 +0800

    components模板

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,18 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  lintOnSave:false,  //关闭语法检查
+
+
+  	//更改默认端口
+	devServer: {
+		open:false,//自动打开浏览器
+		port:9000,	//配置项目启动的规定端口号
+		
+	},
+	
+	pages:{
+index:{
+entry:'src/main.js'
+}}
 })
